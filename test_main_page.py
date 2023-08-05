438d0e7c890007530fe3c3533618812f2814a043
Thrallbata <vicortobdel@gmail.com>
2023-08-05 19:18:28 +0700

    add new test, test_guest_located_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,7 @@
+import time
+
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,10 +10,26 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_page()
+
+
+def test_guest_located_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+    # page.should_be_login_url()
+    # page.should_be_register_form()
+    # page.should_be_login_form()
+
+
+
+
+
 
 
